2cf42b30e801a4adf123653963021277b3866f70
sofia p <sofiiapin26@gmail.com>
2020-05-09 16:13:17 -0500

    burbuja

---
+++ segundo sintoma.cpp
@@ -0,0 +1,18 @@
+   #include<iostream>
+   using namespace std;
+   int main(){
+   	float  n,v=37;
+   	int t;
+   	cout<<"segundo sintoma del covid-19 ingrese su temperatura"<<endl;
+   	cin>>n;
+   	
+   	if(n>=v){
+   		cout<<"\ntienes el segundo sintoma necesias un chequeo"<<endl;
+
+	   }
+   	else{
+   		cout<<"\ncuidate"<<endl;
+	   }
+   	
+   	return 0;
+   }
